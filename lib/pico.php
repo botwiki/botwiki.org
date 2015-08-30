323a3174b93924acb2a550d81fcb5114e5256958
Stefan Bohacek <stefan.bohacek@gmail.com>
2015-08-29 21:46:33 -0400

    Testing.

--- lib/pico.php
+++ lib/pico.php
@@ -346,7 +346,7 @@ class Pico
         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' && $_SERVER['HTTPS'] != '') {
             $protocol = 'https';
         }
-
+        echo $protocol;
         return $protocol;
     }
 
