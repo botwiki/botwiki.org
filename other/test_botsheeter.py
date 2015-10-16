ac654eac440c251c13174d90b53da5630829b3c2
hugovk <hugovk@users.noreply.github.com>
2015-10-16 12:35:57 +0300

    Validate input, add some tests, couple of fixes

---
+++ other/test_botsheeter.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Unit tests for botsheeter.py
+"""
+from __future__ import print_function, unicode_literals
+from hypothesis import given
+from hypothesis.strategies import text
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import botsheeter
+
+
+class TestIt(unittest.TestCase):
+
+    def test_validate_creator_twitter_url_full(self):
+        input = "https://twitter.com/botwikidotorg"
+        output = botsheeter.validate_creator_twitter_url(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_creator_twitter_url_no_https(self):
+        input = "twitter.com/botwikidotorg"
+        output = botsheeter.validate_creator_twitter_url(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_creator_twitter_url_username(self):
+        input = "botwikidotorg"
+        output = botsheeter.validate_creator_twitter_url(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_creator_twitter_url_at_username(self):
+        input = "@botwikidotorg"
+        output = botsheeter.validate_creator_twitter_url(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_creator_twitter_url_username_with_spaces(self):
+        input = "botwiki dot org"
+        output = botsheeter.validate_creator_twitter_url(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_location_url_full(self):
+        input = "https://twitter.com/botwikidotorg"
+        output = botsheeter.validate_location(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_location_no_https(self):
+        input = "twitter.com/botwikidotorg"
+        output = botsheeter.validate_location(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_location_at_username(self):
+        input = "@botwikidotorg"
+        output = botsheeter.validate_location(input)
+        self.assertEqual(output, "https://twitter.com/botwikidotorg")
+
+    def test_validate_location_text(self):
+        """ Can't be sure this is a Twitter username """
+        input = "Snapchat"
+        output = botsheeter.validate_location(input)
+        self.assertEqual(output, "Snapchat")
+
+    def test_validate_location_username_with_spaces(self):
+        """ Can't be sure this is a Twitter username """
+        input = "botwiki dot org"
+        output = botsheeter.validate_location(input)
+        self.assertEqual(output, "botwiki dot org")
+
+
+class TestWithHypothesis(unittest.TestCase):
+
+    @given(text())
+    def test_validate_creator_twitter_url(self, x):
+        output = botsheeter.validate_creator_twitter_url(x)
+        self.assertIsNotNone(output)
+        self.assertIsInstance(output, unicode)
+
+    @given(text())
+    def test_validate_location(self, x):
+        output = botsheeter.validate_location(x)
+        self.assertIsNotNone(output)
+        self.assertIsInstance(output, unicode)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# End of file
