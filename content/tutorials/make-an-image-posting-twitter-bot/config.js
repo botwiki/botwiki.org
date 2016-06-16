eba0487bd0e9ac5eb84833b39c08e8ebaa3c9b4a
Stefan Bohacek <stefan.bohacek@gmail.com>
2016-06-16 08:02:30 -0400

    Cleanup.

--- content/tutorials/make-an-image-posting-twitter-bot/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  consumer_key:         '5xhrGNHMcgn3D4d4F6Q1E5EFk',
-  consumer_secret:      'qfhf2vsc5iswm1ow58aWmQhThXjlBqPdarCHXt95l3hRQrYeiE',
-  access_token:         '723513455861485568-UgQXlTp4lpvuDqHf1BFKt5gpBDNSO7H',
-  access_token_secret:  'suMHrQtpcycj1cyYnIncvV3HwFO3w8yPbyaSyM49IwFEb'
-}
-
-module.exports = config;
