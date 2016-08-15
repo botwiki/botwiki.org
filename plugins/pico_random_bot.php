9b980b4b258c53f108be3c4499ae3e0475e729c1
Stefan Bohacek <stefan@fourtonfish.com>
2016-08-15 00:38:45 -0400

    Added a plugin that navigates to a random bot page.

---
+++ plugins/pico_random_bot.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Redirect to a random bot page.
+*
+* @author Stefan Bohacek
+* @link https://fourtonfish.com
+* @license http://opensource.org/licenses/MIT
+*/
+class Pico_Random_Bot{
+  private $show_random_bot;
+
+  public function __construct(){
+      $this->show_random_bot = false;
+  }
+
+  public function request_url(&$url){
+      if($url == 'random-bot') $this->show_random_bot = true;
+  }
+
+  public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page){
+    if($this->show_random_bot){
+
+      function is_bot_page($page){
+        return(in_array("bot", $page["tags"]));
+      }
+
+      $bot_pages = array_filter($pages, "is_bot_page");
+      $random_bot = array_rand($bot_pages);
+
+      header('Location: ' . $bot_pages[array_keys($bot_pages)[rand(0, count($bot_pages))]]["url"]);
+    }
+  }
+}
