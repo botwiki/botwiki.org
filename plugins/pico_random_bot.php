52f6b08e29185107b6df1d830b163f37c81f6b06
Stefan Bohacek <stefan@fourtonfish.com>
2016-08-15 10:33:41 -0400

    Cleanup.

--- plugins/pico_random_bot.php
+++ plugins/pico_random_bot.php
@@ -10,18 +10,17 @@ class Pico_Random_Bot{
   private $show_random_bot;
 
   public function __construct(){
-      $this->show_random_bot = false;
+    $this->show_random_bot = false;
   }
 
   public function request_url(&$url){
-      if ($url == 'random-bot' || $url == 'random-bot/' ){
-        $this->show_random_bot = true;
-      }
+    if ($url == 'random-bot' || $url == 'random-bot/' ){
+      $this->show_random_bot = true;
+    }
   }
 
   public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page){
     if($this->show_random_bot){
-
       function is_bot_page($page){
         if (is_array($page["tags"])){
           $page_tags = $page["tags"];
@@ -31,7 +30,6 @@ class Pico_Random_Bot{
         }
         return(in_array("bot", $page_tags));
       }
-
       $bot_pages = array_filter($pages, "is_bot_page");
       header('Location: ' . $bot_pages[array_keys($bot_pages)[rand(0, count($bot_pages))]]["url"]);
     }
