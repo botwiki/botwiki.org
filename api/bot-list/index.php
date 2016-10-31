ce910dc27578821527eeef62e29223e54c057135
Stefan Bohacek <stefan@fourtonfish.com>
2016-10-30 22:40:04 -0400

    A quick and dirty solution to the random-bot redirect being slow. Will be improved in the future.

---
+++ api/bot-list/index.php
@@ -0,0 +1,34 @@
+<?php
+// Work in progress. Might need refactoring.
+
+global $bot_list;
+$bot_list = array();
+
+function endsWith($haystack, $needle){
+  $length = strlen($needle);
+  return (substr($haystack, -$length) === $needle);
+}
+
+function getBots($dir){
+  if (endsWith($dir, '/images')){
+    return false;
+  }
+  global $bot_list;
+  $ffs = scandir($dir);
+  foreach($ffs as $ff){
+    if($ff != '.' && $ff != '..'){
+      if (!endsWith($ff, '/index.md') && endsWith($ff, '.md')){
+        $bot_url =  $dir . '/' . $ff ;
+        array_push($bot_list, '"' . substr($bot_url, 7, strlen($bot_url) - 10) . '"');
+      }
+      if (is_dir($dir.'/'.$ff)){
+        getBots($dir.'/'.$ff);
+      }
+    }
+  }
+}
+
+getBots('content/bots');
+
+header('Content-type: application/json');
+echo '[' . implode(',', $bot_list) . ']';
--- plugins/pico_random_bot.php
+++ plugins/pico_random_bot.php
@@ -21,17 +21,28 @@ class Pico_Random_Bot{
 
   public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page){
     if($this->show_random_bot){
-      function is_bot_page($page){
-        if (is_array($page["tags"])){
-          $page_tags = $page["tags"];
-        }
-        else{
-          $page_tags = explode(",", $page["tags"]);
-        }
-        return(in_array("bot", $page_tags));
-      }
-      $bot_pages = array_filter($pages, "is_bot_page");
-      header('Location: ' . $bot_pages[array_keys($bot_pages)[rand(0, count($bot_pages))]]["url"]);
+      // Old solution below can be pretty slow. Current solution could be improved by running the bot-list script with cron every 30-60 minutes to generate a static file, which can then be loaded below.
+
+      // function is_bot_page($page){
+      //   if (is_array($page["tags"])){
+      //     $page_tags = $page["tags"];
+      //   }
+      //   else{
+      //     $page_tags = explode(",", $page["tags"]);
+      //   }
+      //   return(in_array("bot", $page_tags));
+      // }
+      // $bot_pages = array_filter($pages, "is_bot_page");
+      // header('Location: ' . $bot_pages[array_keys($bot_pages)[rand(0, count($bot_pages))]]["url"]);
+
+      ob_start();
+      include(dirname(__FILE__) . '/../api/bot-list/index.php');
+      $output = ob_get_contents();
+      ob_end_clean();
+      $bots = json_decode($output);
+
+      $bot_path = $bots[mt_rand(0, count($bots) - 1)];
+      header('Location: ' . $bot_path);
     }
   }
 }
